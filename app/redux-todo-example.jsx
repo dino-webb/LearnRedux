fb510fd0e76ba24525444797839c6b30cfbbfd54
Dino Adani <dino.adani@crowdstar.com>
2017-07-13 20:32:11 -0700

    Add subscribe and dev tools

--- app/redux-todo-example.jsx
+++ app/redux-todo-example.jsx
@@ -22,13 +22,32 @@ var reducer = (state = stateDefault, action) => {
 
   return state;
 };
-var store = redux.createStore(reducer);
+var store = redux.createStore(reducer, redux.compose(
+  window.devToolsExtension ? window.devToolsExtension() : f => f
+));
 
-console.log('currentState', store.getState());
+var unsubscribe = store.subscribe(() => {
+  var state = store.getState();
+
+  // console.log('searchText is', state.searchText);
+  document.getElementById('app').innerHTML = state.searchText;
+});
+
+// console.log('currentState', store.getState());
 
 store.dispatch({
   type: 'CHANGE_SEARCH_TEXT',
-  searchText: 'Pacifica'
+  searchText: 'Games'
+});
+
+store.dispatch({
+  type: 'CHANGE_SEARCH_TEXT',
+  searchText: 'Work'
+});
+
+store.dispatch({
+  type: 'CHANGE_SEARCH_TEXT',
+  searchText: 'Dog'
 });
 
 console.log('searchText should be Pacifica', store.getState());
